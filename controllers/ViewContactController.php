bf0e7678406fcc150f52414839ccad22fab9f90a
Musk-max <76408168+Musk-max@users.noreply.github.com>
2024-01-23 10:39:14 +0100

    les controllers pour chaque CRUD

---
+++ controllers/ViewContactController.php
@@ -0,0 +1,27 @@
+<?php
+class ViewContactController {
+    private $contactDAO;
+
+    public function __construct(ContactDAO $contactDAO) {
+        $this->contactDAO = $contactDAO;
+    }
+
+    public function viewContact($contactId) {
+        // Récupérer le contact à afficher en utilisant son ID
+        $contact = $this->contactDAO->getById($contactId);
+
+        // Inclure la vue pour afficher les détails du contact
+        include('../views/view_contact.php');
+    }
+}
+
+require_once("../config/config.php");
+require_once("../classes/models/ContactModel.php");
+require_once("../classes/dao/ContactDAO.php");
+$contactDAO=new ContactDAO($pdo);
+$controller=new ViewContactController($contactDAO);
+$controller->viewContact($_GET['id']);
+
+
+?>
+
